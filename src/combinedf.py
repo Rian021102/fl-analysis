57a80019872e75797badfbdcb9b8c5ab2d7f66eb
Rian Rachmanto <108880724+Rian021102@users.noreply.github.com>
2023-11-23 11:46:58 +0800

    adding new updates

--- src/combinedf.py
+++ src/combinedf.py
@@ -10,8 +10,11 @@ print(dfold.head())
 sdd=pd.read_csv('/Users/rianrachmanto/pypro/project/Litho-Fluid-Id/data/raw/sdd_train.csv')
 sdd.drop(['DT'],axis=1,inplace=True)
 print(sdd.head())
+seg=pd.read_csv('/Users/rianrachmanto/pypro/project/Litho-Fluid-Id/data/raw/new_test_seg.csv')
+seg.drop(['DT'],axis=1,inplace=True)
+print(seg.head())
 #merge df2_new and dfold
-df=pd.concat([df2_new,dfold,sdd])
+df=pd.concat([df2_new,dfold,sdd,seg])
 print(df.head())
 
 #export to csv
